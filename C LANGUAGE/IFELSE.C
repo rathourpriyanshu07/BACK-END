e6aded7a06caa2680e9b95aedbe79acb70359039
rathourpriyanshu07 <priyanshurathore326@gmail.com>
2025-07-26 13:42:54 +0530

    first commit

---
+++ C LANGUAGE/IFELSE.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b;
+ clrscr();
+ printf("\nEnter A:");
+ scanf("%d",&a);
+ printf("\nEnter B:");
+ scanf("%d",&b);
+ if(a>b)
+
+   {
+
+    printf("\n A is Greater");
+
+   }
+
+ else
+   {
+   printf("\n B is Greater");
+   }
+   getch();
+   }
