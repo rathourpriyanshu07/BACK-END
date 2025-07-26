2981fcca7f0ae090c24fd3396427729096ee0d13
rathourpriyanshu07 <priyanshurathore326@gmail.com>
2025-07-26 14:09:22 +0530

    first commit

--- C LANGUAGE/PYRAMID.C
+++ C LANGUAGE/PYRAMID.C
@@ -2,15 +2,18 @@
 #include<conio.h>
 void main()
  {
-  int i,j;
+  int i,j,k;
   clrscr();
   for(i=0;i<10;i++)
-  {
-    for(j=0;j<i;j++)
-  {
-    printf("*");
-  }
+   {
+    for(k=0;k<9-i;k++)
+    { printf(" ");
+    }
+    for(j=0;j<=i;j++)
+    {
+     printf(" *");
+    }
     printf("\n");
-  }
+    }
     getch();
- }
+    }
