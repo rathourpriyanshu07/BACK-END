6ecaf80711e8649021fa8ce342baa35ae23ca470
rathourpriyanshu07 <priyanshurathore326@gmail.com>
2025-07-30 14:05:50 +0530

    first commit

---
+++ C LANGUAGE/ADDITION.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c;
+ clrscr();
+ printf("\nEnter First Number:");
+ scanf("%d",&a);
+ printf("\nEnter Second Number:");
+ scanf("%d",&b);
+ c=a+b;
+ printf("\nThe Sum of two Number is: %d",c);
+ getch();
+ }
