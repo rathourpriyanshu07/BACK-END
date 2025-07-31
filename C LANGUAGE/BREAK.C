6437e8dafffb106c59d59ad2f88b2d8845d9b3ea
rathourpriyanshu07 <priyanshurathore326@gmail.com>
2025-07-31 14:34:03 +0530

    first commit

---
+++ C LANGUAGE/BREAK.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int i;
+  clrscr();
+
+  for(i=0;i<10;i++)
+{
+    if(i==5)
+    {
+       break;
+    }
+    else
+    {
+       printf("\nI : %d",i);
+    }
+}
+  getch();
+}
