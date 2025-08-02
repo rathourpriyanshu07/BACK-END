94522ee0b5ddf71b96c67b8229056e2fe54fb6c3
rathourpriyanshu07 <priyanshurathore326@gmail.com>
2025-08-02 13:57:54 +0530

    first commit

---
+++ C LANGUAGE/SUMARRAY.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int a[10],i,sum=0;
+   clrscr();
+   printf("\nEnter Array Elements\n");
+   for(i=0;i<10;i++)
+   {
+	printf(" Enter %d Element : ",i);
+	scanf("%d",&a[i]);
+	sum=sum+a[i];
+   }
+
+   printf("\n Sum : %d",sum);
+   printf("\nArray Element Are\n");
+   for(i=0;i<10;i++)
+   {
+       printf("\n A[%d] = %d",i,a[i]);
+   }
+   getch();
+}
