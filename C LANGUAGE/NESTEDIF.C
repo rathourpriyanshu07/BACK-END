f0def162d7d028d39b19886f09598c2bc8d04eac
rathourpriyanshu07 <priyanshurathore326@gmail.com>
2025-08-07 14:53:03 +0530

    first commit

---
+++ C LANGUAGE/NESTEDIF.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a=10,b=20,c=30;
+  clrscr();
+  if(a<b)
+  {
+  if(a>c)
+  {
+     printf("%d",a);
+  }
+  else
+  {
+     printf("%d",c);
+  }
+  }
+  else
+  {
+  if(b>c)
+  {
+     printf("%d",b);
+  }
+  else
+  {
+  printf("%d",c);
+  }
+  }
+  getch();
+}
