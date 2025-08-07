b67c04ab59d4e94c0d36314d0b0b3cbaf1f935da
rathourpriyanshu07 <priyanshurathore326@gmail.com>
2025-08-07 17:32:44 +0530

    first commit

--- C LANGUAGE/NESTEDIF.C
+++ C LANGUAGE/NESTEDIF.C
@@ -1,30 +1,38 @@
 #include<stdio.h>
 #include<conio.h>
+
 void main()
 {
-  int a=10,b=20,c=30;
-  clrscr();
-  if(a<b)
-  {
-  if(a>c)
-  {
-     printf("%d",a);
-  }
-  else
-  {
-     printf("%d",c);
-  }
-  }
-  else
-  {
-  if(b>c)
-  {
-     printf("%d",b);
-  }
-  else
-  {
-  printf("%d",c);
-  }
-  }
-  getch();
+ int a,b,c;
+ clrscr();
+ printf("\n Enter A Value: ");
+ scanf("%d",&a);
+ printf("\n Enter B Value: ");
+ scanf("%d",&b);
+ printf("\n Enter C Value: ");
+ scanf("%d",&c);
+
+ if(a>b)
+ {
+ if(a>c)
+ {
+    printf("\n A is Greater");
+ }
+ else
+ {
+    printf("\n C is Greater");
+ }
+ }
+ else
+ {
+ if(b>c)
+ {
+    printf("\n B is Greater");
+ }
+ else
+ {
+    printf("\n C is Greater");
+ }
+ }
+ getch();
 }
